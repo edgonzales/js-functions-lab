afc7a836f4a33f83e6cf632c5e9fa648138564c7
Ed Gonzales <edgonzales6@gmail.com>
2023-09-27 13:27:33 -0700

    Lab Problem 1

--- js/app.js
+++ js/app.js
@@ -3,31 +3,47 @@
 // creating the reausable block of code
 // to be used at a later time in the program 
 
-function sayHello(personsName) {
-    // write the code to say hello here
-    console.log(`Hello ${personsName}`)
-}
+// function sayHello(personsName) {
+//     // write the code to say hello here
+//     console.log(`Hello ${personsName}`)
+// }
 
-// execute the function
-// invoke the function
-// call the function
+// // execute the function
+// // invoke the function
+// // call the function
 
-// write the functions name ()
-sayHello('Bob'); // runs the block of code defined by the sayHello func
-sayHello('Pinelli');
+// // write the functions name ()
+// sayHello('Bob'); // runs the block of code defined by the sayHello func
+// sayHello('Pinelli');
 
-function sayGoodbye(phrase, name){
-    console.log(`${phrase}, ${name}`);
-}
+// function sayGoodbye(phrase, name){
+//     console.log(`${phrase}, ${name}`);
+// }
 
-sayGoodbye('Adios', 'Amy');
-sayGoodbye('Sayonara', 'Sam');
-sayGoodbye('Dobrou noc', 'Dan');
+// sayGoodbye('Adios', 'Amy');
+// sayGoodbye('Sayonara', 'Sam');
+// sayGoodbye('Dobrou noc', 'Dan');
 
 
-function add(x, y){
-    //let sum = x + y;
-    return x + y;
-}
-console.log(add(2,2), "<_______ LOOK AT THIS");
+// function add(x, y){
+//     //let sum = x + y;
+//     return x + y;
+// }
+// console.log(add(2,2), "<_______ LOOK AT THIS");
 
+// 1.
+function maxOfTwoNumbers(x, y) {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+    
+    // or more "elegantly" using the fantastic ternary expression!
+    // return  x >= y ? x : y;
+  }
+  
+  console.log(maxOfTwoNumbers(3, 9));
+  
+  // 2.
+  
